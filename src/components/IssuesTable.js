1376e89d53ddfba510780617fca0ff0891c869e3
Oscar Mier <voscarmv@gmail.com>
2022-08-01 17:01:02 -0500

    Issues table component

---
+++ src/components/IssuesTable.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const IssuesTable = ({ issues }) => {
+  if (!issues) {
+    return null;
+  }
+  if (!issues[0]) {
+    return null;
+  }
+  if (!issues[0].url) {
+    return null;
+  }
+  return (
+    <ul>
+      {issues.map(
+        issue => (
+          <li key={ap.id}>
+            <a href={`"${issue.url}"`}>{issue.url}</a>
+          </li>
+        ),
+      )}
+    </ul>
+  );
+};
+
+IssuesTable.propTypes = {
+  issues: propTypes.arrayOf(propTypes.any).isRequired,
+};
+
+export default IssuesTable;
