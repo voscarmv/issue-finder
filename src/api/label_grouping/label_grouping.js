e85c891cec2566c1ea9dcb84744673d9b33bf106
jr-cast <castanos@pm.me>
2022-08-07 18:51:13 -0500

    Fetching, normalizing and storing Y comb repos label names

--- src/api/label_grouping/label_grouping.js
+++ src/api/label_grouping/label_grouping.js
@@ -1,4 +1,15 @@
+// user gh token
 let private_tkn = 'ENTER A VALID TOKEN';
+
+// y_comb_repos raw data  
+const baseURL = 'https://raw.githubusercontent.com/voscarmv/ycombinator_githubs/main/06_repos.json';
+
+// fetch Y combinator main json file
+const fetchRepos = async () => {
+  const conn = await fetch(baseURL);
+  const res = await conn.json().then((data) => data)
+  return res;
+}
 
 // get registered labels from a single repo
 const fetchLabels = async (org, repo) => {
@@ -12,15 +23,19 @@ const fetchLabels = async (org, repo) => {
   return res;
 }
 
-// y_comb_repos raw data endpoint
-const baseURL = 'https://raw.githubusercontent.com/voscarmv/ycombinator_githubs/main/06_repos.json';
-
-// Retrieve repos data
-const fetchRepos = async () => {
-  const conn = await fetch(baseURL);
-  const res = await conn.json().then((data) => data)
-  return res;
-}
+let raw_labels = []; // 1766 at the time of writing this chunk of code
+let norm_labels = []; // 184 after removing repeated label names
 
 // loop thru each item in y_comb list and fetch labels for each item
-fetchRepos().then((data) => data.forEach((item) => fetchLabels(item.org, item.repo))).then((res) => console.log(res));
+fetchRepos().then((res) => res.forEach((item) => fetchLabels(item.org, item.repo)
+  .then((res) => res.forEach((item) => raw_labels.push(item.name)))));
+
+// await for fetching to conclude and remove repeated values from raw_labels
+setTimeout(() => {
+  norm_labels = [...new Set(raw_labels)];
+}, 2500);
+
+// verify normalize label names values
+setTimeout(() => {
+  console.log(norm_labels);
+}, 3000);
