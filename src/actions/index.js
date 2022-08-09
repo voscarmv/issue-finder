3548598263456db719fdc2f6b1482c20fb1fb8f5
jr-cast <castanos@pm.me>
2022-08-09 10:14:15 -0500

    Delete action/types.js to avoid reduncandy; make use of constants files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,21 +1,21 @@
-// import fetch from 'node-fetch';
 import * as api from "../api";
-import types from "./types";
+import * as issue from "../constants/issueConstants"
+import * as label from "../constants/labelConstants"
 
 export const getIssues = (org, repo, label) => async (dispatch) => {
   dispatch({
-    type: types.GET_ISSUES_LOADING
+    type: issue.GET_ISSUES_LOADING
   });
   try {
     const { data } = await api.fetchIssues(org, repo, label);
     console.log(data);
     dispatch({
-      type: types.GET_ISSUES_SUCCESS,
+      type: issue.GET_ISSUES_SUCCESS,
       payload: data,
     });
   } catch (e) {
     dispatch({
-      type: types.GET_ISSUES_ERROR,
+      type: issue.GET_ISSUES_ERROR,
       error: e.message,
     });
   }
@@ -23,17 +23,17 @@ export const getIssues = (org, repo, label) => async (dispatch) => {
 
 export const getLabels = () => async (dispatch) => {
   dispatch({
-    type: types.GET_LABELS_LOADING,
+    type: label.GET_LABELS_LOADING,
   });
   try {
     const data = await api.rawLabels();
     dispatch({
-      type: types.GET_LABELS_SUCCESS,
+      type: label.GET_LABELS_SUCCESS,
       payload: data,
     });
   } catch (e) {
     dispatch({
-      type: types.GET_LABELS_ERROR,
+      type: label.GET_LABELS_ERROR,
       error: e.message,
     });
   }
