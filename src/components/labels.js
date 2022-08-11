d8173ca7a82059737d5899db00e7d0f90b83eb1a
jr-cast <castanos@pm.me>
2022-08-10 21:29:54 -0500

    Add a very basic norm label listing

---
+++ src/components/labels.js
@@ -0,0 +1,22 @@
+const Labels = () => {
+
+  // sample using a bunch of labels (later we can use state selectors)
+  const labels_sample = ['20%-project', 'accessibility', 'backend', 'bug', 'chatwoot-cloud', 'chore', 'content', 'dependencies', 'devops', 'docs-done', 'docs-needed', 'documentation', 'enhancement', 'Enterprise', 'Epic', 'feature', 'frontend', 'Good first issue', 'hacktoberfest', 'hotfix', 'in-QA', 'infrastructure', 'investigation', 'javascript', 'need-design', 'need-discussion', 'need-more-info', 'need-spec', 'on-hold', 'open-for-prs', 'duplicate', 'help wanted', 'invalid', 'question', 'wontfix', '⛵ next-release', 'artilery-pro', 'debt', 'discussion', 'docs', 'engine:http', 'engine:playwright', 'engine:socketio', 'engine:ws', 'engines'];
+
+  return (
+    <ul className="labelCont">
+      {labels_sample.map(
+        label => (
+          <li>
+            <label>
+              <input type="checkbox" />
+              <code>{label}</code>
+            </label>
+          </li>
+        )
+      )}
+    </ul>
+  )
+}
+
+export default Labels;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store'
 import App from './containers/App';
+import Labels from './components/labels';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <App />
+    <Labels />
   </Provider>
 );
 
