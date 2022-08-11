6865f14a68e44e7e554a1f7f92fe64d50626e740
jr-cast <castanos@pm.me>
2022-08-11 13:03:37 -0500

    Fix(temp) true/false logging in label

--- src/components/filtered_basket.js
+++ src/components/filtered_basket.js
@@ -1,8 +1,4 @@
-const filtered_basket = ({ issues }) => {
-
-  // sample using a bunch of labels (later we can use state selectors)
-  const labels_sample = ['20%-project', 'accessibility', 'backend', 'bug', 'chatwoot-cloud', 'chore', 'content', 'dependencies', 'devops', 'docs-done', 'docs-needed', 'documentation', 'enhancement', 'Enterprise', 'Epic', 'feature', 'frontend', 'Good first issue', 'hacktoberfest', 'hotfix', 'in-QA', 'infrastructure', 'investigation', 'javascript', 'need-design', 'need-discussion', 'need-more-info', 'need-spec', 'on-hold', 'open-for-prs', 'duplicate', 'help wanted', 'invalid', 'question', 'wontfix', '⛵ next-release', 'artilery-pro', 'debt', 'discussion', 'docs', 'engine:http', 'engine:playwright', 'engine:socketio', 'engine:ws', 'engines'];
-
+const filtered_basket = () => {
 
   return (
     <ul>
--- src/components/labels.js
+++ src/components/labels.js
@@ -4,7 +4,7 @@ const Labels = () => {
   // sample using a bunch of labels (later we can use state selectors)
   const labels_sample = ['20%-project', 'accessibility', 'backend', 'bug', 'chatwoot-cloud', 'chore', 'content', 'dependencies', 'devops', 'docs-done', 'docs-needed', 'documentation', 'enhancement', 'Enterprise', 'Epic', 'feature', 'frontend', 'Good first issue', 'hacktoberfest', 'hotfix', 'in-QA', 'infrastructure', 'investigation', 'javascript', 'need-design', 'need-discussion', 'need-more-info', 'need-spec', 'on-hold', 'open-for-prs', 'duplicate', 'help wanted', 'invalid', 'question', 'wontfix', '⛵ next-release', 'artilery-pro', 'debt', 'discussion', 'docs', 'engine:http', 'engine:playwright', 'engine:socketio', 'engine:ws', 'engines'];
   // save, get and update state of clicked checkbox
-  const [checked, setChecked] = React.useState(true);
+  // const [checked, setChecked] = React.useState(true);
 
   return (
     <div>
@@ -16,9 +16,7 @@ const Labels = () => {
               <label>
                 <input
                   type="checkbox"
-                  idChecked={checked}
-                  onChange={() => setChecked(!checked)}
-                  onClick={() => console.log(label, checked)}
+                  onClick={(e) => console.log(label, e.target.checked)}
                 />
                 <code>{label}</code>
               </label>
