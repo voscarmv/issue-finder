9266856ba6e0bcfc2e90575a913a6428f76c40ef
Shadman Ali <shadman.ali@live.co.uk>
2022-08-13 02:16:25 +0530

    update: remove issue condition added in select issue reducer

--- src/reducers/selectIssues.js
+++ src/reducers/selectIssues.js
@@ -1,18 +1,21 @@
 import {
-    ADD_TO_SELECT_ISSUES,
-  } from "../constants/selectIssuesConstants";
-  
-  export default function selectIssues(state = [], action) {
-      switch (action.type) {
-        case ADD_TO_SELECT_ISSUES:
-            const selectedIssue = action.payload;
-            const isSelectedIssueExist = state.find(i => i === selectedIssue)
-            if(isSelectedIssueExist) {
-                return state
-            } else {
-                return [...state, selectedIssue]
-            }
-      default:
+  ADD_TO_SELECT_ISSUES,
+  REMOVE_FROM_SELECT_ISSUES,
+} from "../constants/selectIssuesConstants";
+
+export default function selectIssues(state = [], action) {
+  switch (action.type) {
+    case ADD_TO_SELECT_ISSUES:
+      const selectedIssue = action.payload;
+      const isSelectedIssueExist = state.find((i) => i === selectedIssue);
+      if (isSelectedIssueExist) {
         return state;
+      } else {
+        return [...state, selectedIssue];
       }
-    }
+    case REMOVE_FROM_SELECT_ISSUES:
+      return state.filter((i) => i !== action.payload);
+    default:
+      return state;
+  }
+}
