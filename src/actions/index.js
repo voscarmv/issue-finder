fb79d798ad879b11c02c65ff59fa3c56ea48a835
Shadman Ali <shadman.ali@live.co.uk>
2022-08-14 19:39:45 +0530

    list of labels are now stored in local storage to speed up app's loading time, it gets refreshed after a day.

--- src/actions/index.js
+++ src/actions/index.js
@@ -23,7 +23,7 @@ export const getIssues = (org, repo, label) => async (dispatch) => {
   }
 };
 
-export const getLabels = (repos) => async (dispatch) => {
+export const getLabels = (repos) => async (dispatch, getState) => {
   if(repos === undefined) return;
   console.log("Hello world");
   console.log(`inside getLabels ${JSON.stringify(repos)}`);
@@ -31,12 +31,32 @@ export const getLabels = (repos) => async (dispatch) => {
     type: label.GET_LABELS_REQUEST,
   });
   try {
-    const data = await api.rawLabels(repos);
+
+    // get list of labels, saved in browser's local strorage
+    const localDataDate = localStorage.getItem('date');
+    let today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    today = (mm+dd+yyyy).toString();
+    // if date when data was saved is more than a day, clear it.
+    if(localDataDate !== today){
+      localStorage.removeItem('labelslist');
+      localStorage.removeItem('date');
+    }
+
+    // get label list from local storage
+    const localData = localStorage.getItem('labelslist');
+    let data = '';
+    if(localData) data = JSON.parse(localData);
+    else data = await api.rawLabels(repos); // if label list is not present in local stprage than get fresh lists of labels.
     console.log(`inside getLabels 2 ${data}`);
     dispatch({
       type: label.GET_LABELS_SUCCESS,
       payload: data,
     });
+    localStorage.setItem('labelslist', JSON.stringify(getState().labelsStore.labelslist))
+    localStorage.setItem('date',today)
   } catch (e) {
     console.log(`getlabels error ${e.message}`);
     dispatch({
