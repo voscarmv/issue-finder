cdab7a080e5a102e1d75d06c0aa8b41968eb3e06
Ricardo Valtierra <ricardo_valtierra@outlook.com>
2022-08-28 02:55:38 -0500

    Remove src scripts on HTML template that were not found, causing log syntax error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './containers/App';
+import 'tw-elements';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
