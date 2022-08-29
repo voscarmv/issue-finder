e8a5bf3a0d8bd51843eb3e222926bb13bccc6b0a
Shadman Ali <shadman.ali@live.co.uk>
2022-08-29 05:36:17 +0530

    bug fix in 'containers/App.js'
    Issues component was not rendering for menu 2

--- src/containers/App.js
+++ src/containers/App.js
@@ -13,7 +13,7 @@ function App() {
     <>
       <Home />
       {menu === 2 ? <Labels /> : null}
-      {issuesList && menu === 1 ? <Issues /> : null}
+      {issuesList && menu === 1 ? <Issues /> : menu === 2 ? <Issues /> : null}
     </>
   );
 }
