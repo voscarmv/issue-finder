fc581a2e59d92b39df514d7d4fd5830e75e60512
Shadman Ali <shadman.ali@live.co.uk>
2022-09-22 11:04:10 +0530

    made all the necessary changes to make app work with firebase analytics

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-undef */
-import { useEffect } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import pageTrackerAnalytics from './PageTracker';
+import { useLocation } from 'react-router';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -10,6 +11,7 @@ import Labels from '../components/Labels';
 import Issues from '../components/Issues';
 
 function App() {
+  const { pathname, search } = useLocation();
   const analytics = useCallback(() => {
     pageTrackerAnalytics({ path: pathname, search: search, title: pathname.split('/')[1] });
   }, [pathname, search]);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import store from './store';
 import App from './containers/App';
 import 'tw-elements';
@@ -8,6 +9,10 @@ import 'tw-elements';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" exact element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </Provider>
 );
