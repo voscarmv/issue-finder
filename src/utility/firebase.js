dc6001d56f08e3d6324bddf07c4eebe3d00c4af9
Pranjal Maurya <79030501+0xPranjl@users.noreply.github.com>
2022-10-03 07:17:23 +0530

    Update firebase.js

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -8,7 +8,7 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_API_KEY,
+  apiKey:process.env.REACT_APP_FIRBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
   storageBucket:process.env.REACT_APP_STORAGEBUCKET,
