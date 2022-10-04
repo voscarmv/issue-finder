46d08b16488c7b2b80b0fd2bd019f73b89a35cac
Shadman Ali <shadman.ali@live.co.uk>
2022-10-04 10:56:46 +0530

    Update firebase.js
    linter fix

--- src/utility/firebase.js
+++ src/utility/firebase.js
@@ -8,11 +8,11 @@ import { getAnalytics } from 'firebase/analytics';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey:process.env.REACT_APP_FIRBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIRBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
   projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket:process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId:process.env.REACT_APP_MSG_SENDERID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MSG_SENDERID,
   appId: process.env.REACT_APP_APPID,
   measurementId: process.env.REACT_APP_MEASUREMENTID
 };
