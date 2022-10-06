6bf2c60a9af2f4fc79c44ed6181fec8ae968f80d
Ákos Barabás <akos.barabas2002@gmail.com>
2022-10-06 11:08:16 +0200

    fixed format single quotes & semicolon

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from "../containers/App"
+import App from '../containers/App';
 
 test('renders learn react link', () => {
   render(<App />);
